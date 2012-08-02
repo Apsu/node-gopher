d85c24bc8a86345db302e14806906cb7c1ba3ae0
Brendan LaMarche <drac@dracsoft.com>
2012-08-02 12:39:52 -0400

    more features/test

--- tests/test-server1.js
+++ tests/test-server1.js
@@ -12,6 +12,28 @@ var server = gopher.createServer({
     "request": onRequest
 });
 
+server.hook("/HookTest", hookTest);
+server.hook("/Jpeg", jpegHook);
+server.hook("/Png", pngHook);
+
+
+
+//Callbacks
+function jpegHook(client, request) {
+    client.send(fs.createReadStream("./tests/test-jpeg.jpg"));
+}
+
+function pngHook(client, request) {
+    client.send(fs.createReadStream("./tests/test-png.png"));
+}
+
+function hookTest(client, request) {
+    console.log("HOOK: "+request.path);
+    client.send(
+        server.menuLine("i", "Hook successful", "")
+    );
+}
+
 function onConnect(client) {
     console.log("CONNECT: "+client);;
 }
@@ -28,34 +50,40 @@ function onRequest(client, request) {
     try {
         console.log("REQ: "+request.path);
         
-        if (request.path!="")
+        if (!request.hooked)
         {
-            switch (request.path)
+            if (request.path!="")
             {
-                case "/Search":
-                    client.send(
-                        server.menuLine("i", "Search results for "+request.query+":", "") +
-                        server.menuLine("7", "Search Again", "/Search") +
-                        server.menuLine("0", "Some result", "/Textdoc")
-                        );
-                    break;
-                
-                case "/Textdoc":
-                    client.send(fs.createReadStream("./package.json", { "encoding": 'utf8' }));
-                    break;
+                switch (request.path)
+                {
+                    case "/Search":
+                        client.send(
+                            server.menuLine("i", "Search results for "+request.query+":", "") +
+                            server.menuLine("7", "Search Again", "/Search") +
+                            server.menuLine("0", "Some result", "/Textdoc")
+                            );
+                        break;
                     
-                default:
-                    client.send(server.menuLine("3", "ERROR - Not Found: "+request.path, ""));
-                    break;
+                    case "/Textdoc":
+                        client.send(fs.createReadStream("./package.json"));//, { "encoding": 'utf8' }
+                        break;
+                        
+                    default:
+                        client.send(server.menuLine("3", "ERROR - Not Found: "+request.path, ""));
+                        break;
+                }
             }
-        }
-        else
-        {
-            client.send(
-                server.menuLine("i", "Default Menu", "") +
-                server.menuLine("7", "Search", "/Search") +
-                server.menuLine("0", "Textdoc", "/Textdoc")
-                );
+            else
+            {
+                client.send(
+                    server.menuLine("i", "Default Menu", "") +
+                    server.menuLine("7", "Search", "/Search") +
+                    server.menuLine("0", "Textdoc", "/Textdoc") +
+                    server.menuLine("I", "Image (Png)", "/Png") +
+                    server.menuLine("I", "Image (Jpeg)", "/Jpeg") +
+                    server.menuLine("1", "Hook Test", "/HookTest")
+                    );
+            }
         }
     }
     catch (err) {
